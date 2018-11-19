f7a358276ba80dc52bbaf7d53c43b3107fe6c552
HILL\greg <grzegorz.janoszek@gmail.com>
2018-11-19 23:38:21 +0100

    page 53

---
+++ SampleApp/SampleApp/Controllers/Repository/GuestRepository.cs
@@ -0,0 +1,28 @@
+﻿using SampleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApp.Controllers.Repository
+{
+    public static class GuestRepository
+    {
+        private static List<Guest> guests = new List<Guest>();
+
+        public static IEnumerable<Guest> AllGuests
+        {
+            get
+            {
+                return guests;
+            }
+        }
+
+        public static void AddGuest(Guest guest)
+        {
+            guests.Add(guest);
+        }
+            
+
+    }
+}
